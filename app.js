2496e4370ece953dd8c657eb13190c3b69531dbc
tim-lee92 <tim-lee92@hotmail.com>
2017-03-07 21:10:48 -0800

    deploying

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set( 'port', ( process.env.PORT || 3000 ));
 
 app.listen( app.get('port'), function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
